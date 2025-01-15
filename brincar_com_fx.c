82850f07771ce5cd673d584f1065cb7e1f698d65
Tomaz Ferreira De Lima <toferrei@c2r4s2.42lisboa.com>
2025-01-15 17:21:17 +0000

    mini ls ex

--- brincar_com_fx.c
+++ brincar_com_fx.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
 
-static char *go_up_directory(char *str)
+char *go_up_directory(char *str)
 {
 	int n = 0;
+	int i = 0;
 	char *res;
 	while (str[n])
 		n++;
 	while (str[n] != '/')
 	{
-
+		n--;
 	}
-	return (str);
+	res = malloc (n + 10);
+	strncpy(res, str, n);
+	return (res);
+}
+
+void ls(char *path)
+{
+	DIR *dir;
+	struct dirent *entry;
+	dir = opendir(path);  // Specify the directory path
+    if (dir == NULL) {
+        perror("opendir");
+        return ;  // Error opening directory
+    }
+
+    // Read entries in the directory
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);  // Print the name of each directory entry
+    }
+
+    // Close the directory
+    closedir(dir);
 }
 
 int main(void)
 {
-	char *arr;
+	char arr[1000];
 	char *new_arr;
+	int i = 0;
+	
 
-	arr = malloc (1000);
-	printf("\n %s \n", getcwd(arr, 1000));
+	printf("\n%s\n", getcwd(arr, 1000));
 	new_arr = go_up_directory(arr);
+	printf("\n%s\n", new_arr);
 	chdir(new_arr);
+	printf("\n%s\n", getcwd(arr, 1000));
+	ls(arr);
 }
