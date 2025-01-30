510caf99c3fdda4076f403754a1473620d0c52ec
fabio <fabio4_8@hotmail.com>
2025-01-30 18:30:47 +0000

    2º tokenization done

--- second_tokenization.c
+++ second_tokenization.c
@@ -6,7 +6,7 @@
 /*   By: famendes <famendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:16:54 by famendes          #+#    #+#             */
-/*   Updated: 2025/01/29 21:38:15 by famendes         ###   ########.fr       */
+/*   Updated: 2025/01/30 18:30:25 by famendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static char	*has_expand(char *str)
 	}
 	return (NULL);
 }
-// AQUI
+
 static t_token *extract_operator(t_token *token, char *operator)
 {
 	t_token *new;
@@ -57,32 +57,56 @@ static t_token *extract_operator(t_token *token, char *operator)
 	int	op_len;
 
 	op_len = ft_strlen(operator);
-	printf("a legnht é: %d\n", op_len);
 	new_value = ft_substr(token->value, 0, op_len);
 	new = init_token(new_value);
 	new_value = ft_substr(token->value, op_len, ft_strlen(token->value) - op_len);
 	new_after = init_token(new_value);
-	new->previous = token->previous;
+	if (!new && !new_after)
+		return (NULL);
 	new->next = new_after;
 	new_after->previous = new;
-	new_after->next = token->next;
+	if (token->previous)
+		token->previous->next = new;
+	if (token->next)
+		token->next->previous = new_after;
 	free(token->value);
 	free(token);
 	return(new);
 }
-static void	extract_word(t_token *token)
+static t_token *extract_word(t_token *token, char *operator)
 {
-	printf("FAZER AMANHA\n\n\n\n");
+	int	i;
+	t_token *new;
+	t_token	*new_after;
+	char *new_value;
+
+	i = 0;
+   while (token->value[i] && ft_strncmp(&token->value[i],
+   			operator, ft_strlen(operator)) != 0)
+        i++;
+	new_value = ft_substr(token->value, 0, i);
+	new = init_token(new_value);
+	new_value = ft_substr(token->value, i, ft_strlen(token->value) -  i);
+	new_after = init_token(new_value);
+	if (!new && !new_after)
+		return (NULL);
+	new->next = new_after;
+	new_after->previous = new;
+	if (token->previous)
+		token->previous->next = new;
+	if (token->next)
+		token->next->previous = new_after;
+	free(token->value);
+	free(token);
+	return(new);
 }
 
-void	reasign_tokens(t_token *token, char *operator)
+static void	*reasign_tokens(t_token **token, char *operator)
 {
-	t_token	*new_first;
-
-	if (ft_strncmp(token->value, operator, ft_strlen(operator) + 1))
-		new_first = extract_operator(token, operator);
+	if (ft_strncmp((*token)->value, operator, ft_strlen(operator)) == 0)
+		(*token) = extract_operator((*token), operator);
 	else
-		extract_word(token);
+		(*token) = extract_word((*token), operator);
 }
 
 void	second_tokenazor(t_token *token)
@@ -96,11 +120,8 @@ void	second_tokenazor(t_token *token)
 	{
 		operator = has_operator(token->value);
 		if (operator && !in_quotes(token->value, 1))
-			reasign_tokens(token, operator);
-/* 		expand = has_expand(token->value);
-		if (expand || (expand && double_quotes(token->value, 1)))
-			//todo(token, expand);
-		token->index = i++; */
+			reasign_tokens(&token, operator);
+		token->index = i++;
 		token = token->next;
 	}
 }
