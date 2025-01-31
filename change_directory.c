cb4761617b74ee7b7f2cc46c00f0e4f6f0764b90
tomazz casa <etom@PcdoTomaz>
2025-01-31 02:45:52 +0000

    cd wip

--- change_directory.c
+++ change_directory.c
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:26:16 by toferrei          #+#    #+#             */
-/*   Updated: 2025/01/30 19:09:59 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/01/31 02:43:43 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,13 @@ size_t size_until_symbol(char * str, char c)
 	return (n);
 }
 
+char *get_value_for_list(char * str)
+{
+	while (*str != '=')
+		str++;
+	return (str);
+}
+
 void	env_to_list(t_data *data, char **env)
 {
 	t_env *temp;
@@ -96,28 +103,28 @@ void	env_to_list(t_data *data, char **env)
 	int n = 0;
 	data->env = malloc(sizeof * data->env);
 	*data->env = NULL;
-	// temp2 = malloc (1100);
 	while (env[n])
 	{
-		temp1 = malloc (1100);
+		temp1 = malloc (ft_strlen(env[n] + 1));
+		printf("%ld\n", size_until_symbol(env[n], '=') + 1);
 		ft_strlcpy(temp1, env[n], size_until_symbol(env[n], '=') + 1);
 		printf("%s\n", temp1);
-		temp = ft_newnode(temp1, "55555");
+		temp = ft_newnode(temp1, get_value_for_list(env[n]));
 		ft_modified_lstadd_back(data->env, temp);
 		n++;
 		free (temp1);
 	}
-	// free (temp2);
 }
 
 char *get_var_value(t_env *env, char *var_name)
 {
 	while (env != NULL)
 	{
-		if (!ft_strncmp(env->name, var_name, ft_strlen(env->name)))
+		if (!ft_strncmp(env->name, var_name, ft_strlen(var_name)))
 		{
 			return (env->value);
 		}
+		env= env->next;
 	}
 	return (NULL);
 }
@@ -135,10 +142,13 @@ static void	update_pwd(t_data *data)
 	}
 }
 
+
+
 void change_directory(char **args, int fd, t_data *data)
 {
 	char curpath[4096]; //change to PATH_MAX macro
 
+	curpath[0] = '\0';
 	if (!args[1] && !get_var_value(*(data->env), "HOME"))
 	{
 		perror("cd : erro de nao have home"); //to do (write)
@@ -148,7 +158,7 @@ void change_directory(char **args, int fd, t_data *data)
 	else if((!args[1] && data->home) || !ft_strncmp(args[1], "~", ft_strlen(args[1])) || !ft_strncmp(args[1], "~/", ft_strlen("~/")))
 	{
 		ft_strlcat(curpath, data->home, ft_strlen(data->home) + 1);
-		if (!ft_strncmp(args[1], "~/", ft_strlen("~/")) && ft_strncmp(args[1], "~/", ft_strlen(args[1])))
+		if (ft_strncmp(args[1], "~", ft_strlen("~")) && ft_strncmp(args[1], "~/", ft_strlen(args[1])))
 			ft_strlcat(curpath, data->pwd, ft_strlen(data->pwd) + 1);
 	}
 	else if(!ft_strncmp(args[1], "-", ft_strlen("-")))
@@ -162,9 +172,17 @@ void change_directory(char **args, int fd, t_data *data)
 		else
 			ft_strlcat(curpath, get_var_value(*(data->env), "OLDPWD"), ft_strlen(get_var_value(*(data->env), "OLDPWD")) + 1);
 	}
-	else if(ft_strncmp(args[1], "/", ft_strlen("/")))
+	else if(!ft_strncmp(args[1], "/", ft_strlen("/")) ||
+		!ft_strncmp(args[1], ".", ft_strlen(args[1])) ||
+		!ft_strncmp(args[1], "..", ft_strlen(args[1])))
+	{
+		ft_strlcat(curpath, args[1], ft_strlen(args[1] + 1));
+	}
+	else
 	{
 		ft_strlcat(curpath, data->pwd, ft_strlen(data->pwd) + 1);
+		ft_strlcat(curpath, "/", ft_strlen(data->pwd) + 3);
+		ft_strlcat(curpath, args[1], ft_strlen(args[1]) + ft_strlen(data->pwd) + 2);
 	}
 	printf("olaolaola: %s\n", curpath);
 	// export(); // to do
@@ -181,6 +199,7 @@ char **args_creator(char **av)
 	result[1] = malloc (sizeof * result * ft_strlen(av[2]));
 	ft_strlcpy(result[0], av[1], 1000);
 	ft_strlcpy(result[1], av[2], 1000);
+
 	return (result);
 }
 
@@ -189,13 +208,13 @@ int	main(int ac, char **av, char **env)
 	char **args;
 	t_data data;
 
+	data.pwd = "/home/etom/Escola42/minishell";
 	if (ac != 3)
 	{
 		printf( "deu cagada, o bjoetivo e tipo have uma string ./a.out cd [qualquer string aqui]");
 		return 0;
 	}
 	args = args_creator(av);
-	write (1, "ola\n", 5);
 	int n = 0;
 	env_to_list(&data, env);
 	ft_print_list(data.env);
