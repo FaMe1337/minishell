b0d3c603f35da4239a1da87ee6c73120e25349f8
fabio <fabio4_8@hotmail.com>
2025-02-16 23:03:36 +0000

    executor started

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: famendes <famendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:29:20 by famendes          #+#    #+#             */
-/*   Updated: 2025/02/16 19:34:14 by famendes         ###   ########.fr       */
+/*   Updated: 2025/02/16 22:57:11 by famendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ void	show_starter(char **env, t_data *data)
 	while (1)
 	{
 		data->input = readline(data->pwd_with_till);
-		if (!data->input)
+		if (!data->input || !data->input[0])
 		{
-			perror("Error with readline\n");
+			perror("Empty string\n");
 			break;
 		}
 		else
 		{
 			add_history(data->input);
 			if (input_parser(data) == 1)
-				//executor(data);
-				printf("%s\n", "sucess");
+				executor(data);
+				//printf("%s\n", "sucess");
 			else
 			{
 				printf("%s\n", "failed parsing");
--- redirections_handler.c
+++ redirections_handler.c
@@ -6,44 +6,68 @@
 /*   By: famendes <famendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 20:51:25 by famendes          #+#    #+#             */
-/*   Updated: 2025/02/16 22:21:44 by famendes         ###   ########.fr       */
+/*   Updated: 2025/02/16 23:01:41 by famendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	red_out(char *str, t_pipe *cmd)
+int	append(char *str, t_pipe *cmd)
 {
 	int		fd;
-	char	*filename;
-
-	if (!str && !str[0])
+	if (!str || !str[0])
 	{
 		ft_putstr_fd("No outfile present\n", 2);
 		cmd->bad_fd = true;
 		return (1);
 	}
-	filename = str + 4;
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd = open(str, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd < 0)
 	{
 		ft_putstr_fd("Invalid outfile\n", 2);
 		cmd->bad_fd = true;
 		return (1);
 	}
+	if (cmd->fd_out >= 0)
+		close(cmd->fd_out);
+	cmd->fd_out = fd;
+	return (0);
+}
+
+int	red_out(char *str, t_pipe *cmd)
+{
+	int		fd;
+	if (!str || !str[0])
+	{
+		ft_putstr_fd("No outfile present\n", 2);
+		cmd->bad_fd = true;
+		return (1);
+	}
+	fd = open(str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+	{
+		ft_putstr_fd("Invalid outfile\n", 2);
+		cmd->bad_fd = true;
+		return (1);
+	}
+	if (cmd->fd_out >= 0)
+		close(cmd->fd_out);
+	cmd->fd_out = fd;
 	return (0);
 }
 
 static int	parse_redirections(char *str, t_pipe *cmd)
 {
 	if (ft_strncmp(str, "DOC:", 4) == 0)
-		here_doc(str, cmd);
+		//here_doc(str + 4, cmd);
+		printf("ola");
 	else if (ft_strncmp(str, "RDI:", 4) == 0)
-		red_out(str, cmd);
+		//red_in(str + 4, cmd);
+		printf("ola");
 	else if (ft_strncmp(str, "RDO:", 4) == 0)
-		red_out(str, cmd);
+		red_out(str + 4, cmd);
 	else if (ft_strncmp(str, "APP:", 4) == 0)
-		append(str, cmd);
+		append(str + 4, cmd);
 	if (cmd->bad_fd)
 		return (1);
 	return (0);
